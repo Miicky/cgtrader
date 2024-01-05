848a1ef235cba455b04dfb6d8aaac8c12df0504e
Micky <micky.ukr@gmail.com>
2024-01-05 11:31:05 +0200

    Fix initialize rubocop issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Centralized view-related methods storage
 module ApplicationHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# Organize email-related methods centrally.
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
