8eb579feed1562b7cf2bf635c8009f5755e71144
Micky <micky.ukr@gmail.com>
2024-01-06 12:55:06 +0200

    Add test for rake task

--- spec/rake/import_dataset_spec.rb
+++ spec/rake/import_dataset_spec.rb
@@ -5,15 +5,17 @@ require 'rails_helper'
 Rails.application.load_tasks
 
 describe 'Import dataset' do
-  it 'not raised error' do
-    expect { Rake::Task['dataset:import'].invoke }.not_to raise_error
+  let(:prompts_count) { 180 }
+  let(:dataset_stub) { DatasetStub.new(prompts_count) }
+
+  before do
+    # TODO: make stun dynamic by offset and length
+    dataset_stub.dataset
+    dataset_stub.dataset(offset: 100)
+    Rake::Task['dataset:import'].invoke
   end
 
-  describe 'Creates Prompts' do
-    before { Rake::Task['dataset:import'].invoke }
-
-    it 'successfully create two prompts' do
-      expect(Prompt.count).to eq 2
-    end
+  it 'successfully create two prompts' do
+    expect(Prompt.count).to eq 180
   end
 end
