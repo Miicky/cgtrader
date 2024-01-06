15cd776ba1c23901c6e39236ef2f61176e7988a7
Micky <micky.ukr@gmail.com>
2024-01-06 13:16:44 +0200

    Add logs to dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  logger = ActiveSupport::Logger.new($stdout)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
 
   # Do not eager load code on boot.
   config.eager_load = false
