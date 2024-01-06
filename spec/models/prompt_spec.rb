8706cb506fe543035e40a882e5660a1657dd3da2
Micky <micky.ukr@gmail.com>
2024-01-06 17:36:01 +0200

    Add spec model for searching

--- spec/models/prompt_spec.rb
+++ spec/models/prompt_spec.rb
@@ -7,4 +7,22 @@ describe Prompt do
     prompt = described_class.new
     expect(prompt).to be_valid
   end
+
+  describe '#seach' do
+    let(:prompt_first) { 'My first prompt' }
+    let(:prompt_second) { 'My prompt searchable' }
+    let(:prompt_third) { 'My prompt searchable big text first' }
+    let(:prompt_fourth) { 'First rails' }
+    let(:prompt_fifth) { 'Unknown text' }
+    let(:prompts) { [prompt_first, prompt_second, prompt_third, prompt_fourth, prompt_fifth] }
+    let(:search) { 'first prompt' }
+
+    # TODO: split expectations into different examples
+    it 'Returns expected prompts' do
+      prompts.each { |text| described_class.create(text:) }
+      expect(described_class.search(search).count).to eq(4)
+      expect(described_class.search(search).first.text).to eq(prompt_first)
+      expect(described_class.search(search)[1].text).to eq(prompt_third)
+    end
+  end
 end
