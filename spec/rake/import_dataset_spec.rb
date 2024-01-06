b96f5a7942a559a6c8f395894cd802483774a0a4
Micky <micky.ukr@gmail.com>
2024-01-06 11:46:15 +0200

    Add test for rake with Creating two Prompts

--- spec/rake/import_dataset_spec.rb
+++ spec/rake/import_dataset_spec.rb
@@ -8,4 +8,12 @@ describe 'Import dataset' do
   it 'not raised error' do
     expect { Rake::Task['dataset:import'].invoke }.not_to raise_error
   end
+
+  describe 'Creates Prompts' do
+    before { Rake::Task['dataset:import'].invoke }
+
+    it 'successfully create two prompts' do
+      expect(Prompt.count).to eq 2
+    end
+  end
 end
