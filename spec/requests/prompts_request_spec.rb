7e7450599d973490c34507afe6d925748fd4a840
Micky <micky.ukr@gmail.com>
2024-01-07 12:31:10 +0200

    Add request test for searching

--- spec/requests/prompts_request_spec.rb
+++ spec/requests/prompts_request_spec.rb
@@ -8,5 +8,20 @@ describe 'Prompts' do
       get prompts_path
       expect(response).to have_http_status(:ok)
     end
+
+    describe '#search' do
+      let!(:prompt_searchable) { create(:prompt) }
+      let!(:prompt_not_searchable) { create(:prompt) }
+
+      before { get prompts_path, params: { search: prompt_searchable.text } }
+
+      it 'shows searchable prompt' do
+        expect(response.body).to include(prompt_searchable.text)
+      end
+
+      it "doesn't show not_searchable prompt" do
+        expect(response.body).not_to include(prompt_not_searchable.text)
+      end
+    end
   end
 end
