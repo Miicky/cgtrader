11db1c4410824fc260e44b63922ce9620e48e778
Micky <micky.ukr@gmail.com>
2024-01-07 14:47:59 +0200

    Downgrade version

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+
 Elasticsearch::Model.client = Elasticsearch::Client.new(
   port: ENV.fetch('ES_PORT', 9200),
-  host: ENV.fetch('ES_HOST', 'http://elasticsearch')
+  host: ENV.fetch('ES_HOST', 'http://elasticsearch'),
+  user: ENV.fetch('ES_USER','elastic'),
+  password: ENV.fetch('ES_PASSWORD','none')
 )
