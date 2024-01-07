e2fd852c4c8cd8c952eb770cec250aeffaec233a
Micky <micky.ukr@gmail.com>
2024-01-07 11:36:59 +0200

    Add test for rendering list

--- spec/views/prompts/index.html.erb_spec.rb
+++ spec/views/prompts/index.html.erb_spec.rb
@@ -3,8 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'prompts/index' do
-  it 'renders am index page' do
+  it 'renders an index page' do
     render
     expect(rendered).to match(/Prompts#index/)
+  end
+
+  describe 'renders all prompts' do
+    let(:prompts) { create_list(:prompt, 2) }
+
+    it 'render two prompts' do
+      render
+      prompts.each do |prompt|
+        assert_select 'tr>td', text: prompt.text, count: 1
+      end
+    end
   end
 end
