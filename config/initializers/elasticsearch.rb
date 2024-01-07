ca9a8530f9ab309dc3043ff36de5443e90ad6cbb
Micky <micky.ukr@gmail.com>
2024-01-07 15:24:57 +0200

    Improve elastic config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-
-Elasticsearch::Model.client = Elasticsearch::Client.new(
+elastic_main_config = {
   port: ENV.fetch('ES_PORT', 9200),
-  host: ENV.fetch('ES_HOST', 'http://elasticsearch'),
-  user: ENV.fetch('ES_USER','elastic'),
-  password: ENV.fetch('ES_PASSWORD','none')
-)
+  host: ENV.fetch('ES_HOST', 'http://elasticsearch')
+}
+
+elastic_prod_config = {
+  user: ENV.fetch('ES_USER', 'elastic'),
+  password: ENV.fetch('ES_PASSWORD', 'none')
+}
+
+full_elastic_main_config = Rails.env.production? ? elastic_main_config.merge(elastic_prod_config) : elastic_main_config
+Elasticsearch::Model.client = Elasticsearch::Client.new(full_elastic_main_config)
