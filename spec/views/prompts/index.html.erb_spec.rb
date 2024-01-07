236a8796bcd585149a60c6ac5861614dfd387540
Micky <micky.ukr@gmail.com>
2024-01-07 12:17:03 +0200

    Fix test

--- spec/views/prompts/index.html.erb_spec.rb
+++ spec/views/prompts/index.html.erb_spec.rb
@@ -3,19 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'prompts/index' do
-  it 'renders an index page' do
+  let!(:prompts) { create_list(:prompt, 2) }
+
+  before do
+    assign(:prompts, prompts)
     render
+  end
+
+  it 'renders an index page' do
     expect(rendered).to match(/Prompts#index/)
   end
 
-  describe 'renders all prompts' do
-    let(:prompts) { create_list(:prompt, 2) }
-
-    it 'render two prompts' do
-      render
-      prompts.each do |prompt|
-        assert_select 'tr>td', text: prompt.text, count: 1
-      end
+  it 'render two prompts' do
+    prompts.each do |prompt|
+      assert_select 'tr>td', text: prompt.text, count: 1
     end
   end
 end
